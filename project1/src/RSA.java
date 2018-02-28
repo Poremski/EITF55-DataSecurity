3dcbc55ba6075cbcfd070effd34088d0c287a682
theIDinside <simon.farre.cx@gmail.com>
2018-02-28 22:14:27 +0100

    Assignment 5 init, restructure RSA class to hold encryption keys, and move main out of the class into App

--- project1/src/RSA.java
+++ project1/src/RSA.java
@@ -12,40 +12,45 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import static java.math.BigInteger.*;
 
-public class RSA {
+interface BigIntGenerate {
+    BigInteger randomStep(BigInteger b);
+}
 
-    interface BigIntGenerate {
-        BigInteger randomStep(BigInteger b);
-    }
-
-    public static final BigInteger TWO = ONE.add(ONE);
+class App {
     public static void main(String[] args) {
         /**
          *  Assignment 2.1 & 2.3 & 2.4
          */
         // 100 consecutive 512-bit primes
-        List<BigInteger> consecutive512bitPrimes = generate_100_primes(512, (num) -> num.add(TWO));
+        List<BigInteger> consecutive512bitPrimes = RSA.generate_100_primes(512, (num) -> num.add(TWO));
         // 100 randomized 512-bit primes
-        List<BigInteger> random512bitPrimes = generate_100_primes(512, (n) -> {
+        List<BigInteger> random512bitPrimes = RSA.generate_100_primes(512, (n) -> {
             BigInteger bi = new BigInteger(512, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
 
         // 100 randomized 1024-bit primes
-        List<BigInteger> random1024bitPrimes = generate_100_primes(1024, (n) -> {
+        List<BigInteger> random1024bitPrimes = RSA.generate_100_primes(1024, (n) -> {
             BigInteger bi = new BigInteger(1024, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
 
         // 100 randomized 2048-bit primes
-        List<BigInteger> random2048bitPrimes = generate_100_primes(2048, (n) -> {
+        List<BigInteger> random2048bitPrimes = RSA.generate_100_primes(2048, (n) -> {
             BigInteger bi = new BigInteger(2048, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
         System.out.println("Done.");
     }
+}
 
-    private static List<BigInteger> generate_100_primes(int bitSize, BigIntGenerate big) {
+public class RSA {
+
+    public BigInteger p, q, d;
+    public long exp;    // exponent e
+
+    public static final BigInteger TWO = ONE.add(ONE);
+    static List<BigInteger> generate_100_primes(int bitSize, BigIntGenerate big) {
         BigInteger begin;
         do
             begin = new BigInteger(bitSize, new Random(System.nanoTime() ^ Long.MAX_VALUE));
@@ -60,7 +65,6 @@ public class RSA {
         System.out.println(String.format("it took %d ms to generate 100 %d-bit primes, avg: %d ms/prime", (endTime-startTime)/1000000, bitSize, (endTime-startTime)/(1000000*100)));
         return primes;
     }
-
     private static boolean isMillerRabin(BigInteger n) {
         if(n.getLowestSetBit() != 0) {
             System.out.println("isMillerRabin: false");
@@ -109,4 +113,7 @@ public class RSA {
         }
     }
 
+    private static BigInteger private_exponent() {
+
+    }
 }
