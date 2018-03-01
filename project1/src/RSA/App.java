2b3200a079c55d1a0dbb330d161876aa81b1479d
theIDinside <simon.farre.cx@gmail.com>
2018-03-01 19:55:56 +0100

    Parametergeneration seems to work, as well as encrypting + decrypting.
    Has a little cleaning up to do, then I am done.

--- project1/src/RSA/App.java
+++ project1/src/RSA/App.java
@@ -1,6 +1,14 @@
 package RSA;
-import static java.math.BigInteger.valueOf;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+import static java.math.BigInteger.valueOf;
+import static java.lang.System.nanoTime;
 public class App {
     public static void main(String[] args) {
         /**
@@ -8,27 +16,69 @@ public class App {
          */
         /*
         // 100 consecutive 512-bit primes
-        List<BigInteger> consecutive512bitPrimes = RSA.generate_100_primes(512, (num) -> num.add(TWO));
+        List<BigInteger> consecutive512bitPrimes = RSA.test_generate_100_primes(512, (num) -> num.add(TWO));
         // 100 randomized 512-bit primes
-        List<BigInteger> random512bitPrimes = RSA.generate_100_primes(512, (n) -> {
+        List<BigInteger> random512bitPrimes = RSA.test_generate_100_primes(512, (n) -> {
             BigInteger bi = new BigInteger(512, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
 
         // 100 randomized 1024-bit primes
-        List<BigInteger> random1024bitPrimes = RSA.generate_100_primes(1024, (n) -> {
+        List<BigInteger> random1024bitPrimes = RSA.test_generate_100_primes(1024, (n) -> {
             BigInteger bi = new BigInteger(1024, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
 
         // 100 randomized 2048-bit primes
-        List<BigInteger> random2048bitPrimes = RSA.generate_100_primes(2048, (n) -> {
+        List<BigInteger> random2048bitPrimes = RSA.test_generate_100_primes(2048, (n) -> {
             BigInteger bi = new BigInteger(2048, new Random(Long.MAX_VALUE ^ System.currentTimeMillis()));
             return bi.getLowestSetBit() == 0 ? bi : bi.subtract(ONE);
         });
         System.out.println("Done generating all prime numbers.");
         */
+        RSA rsa = null;
+        try {
+            rsa = new RSA(512);
+            Random random = new Random(Long.MIN_VALUE ^ nanoTime());
+            BigInteger s;
+            do {
+                s = new BigInteger(512, random);
+            } while(s.compareTo(ONE) < 1
+                || s.compareTo(rsa.getN()) > 1);
 
-        System.out.println(RSA.modInversem(valueOf(42), valueOf(2017)).toString());
+            BigInteger c = rsa.encrypt(s);
+            BigInteger d = rsa.decrypt(c);
+            System.out.println(String.format("s = %s \nd = %s \nc= %s", s, d, c));
+            System.out.println(String.format("Bitsizes: s is: %d \nc = %d \nd= %d \nN = %d", s.bitLength(), c.bitLength(), d.bitLength(), rsa.getN().bitLength()));
+            if(d.compareTo(s) == 0) {
+                System.out.println("Encryption of s succeeded, since s === d");
+            } else {
+                System.out.println("Encryption of s FAILED, since s !== d");
+            }
+            System.out.println(rsa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BigInteger gcd(BigInteger m, BigInteger n) {
+        if(n.compareTo(ZERO) == 0) return m;
+        else return gcd(n, m.mod(n));
+    }
+
+    static List<BigInteger> test_generate_100_primes(int bitSize, BigIntGenerate big) {
+        BigInteger begin;
+        do
+            begin = new BigInteger(bitSize, new Random(nanoTime() ^ Long.MAX_VALUE));
+        while(begin.getLowestSetBit() != 0);
+        Long startTime = nanoTime();
+        List<BigInteger> primes = Stream.iterate(begin, big::randomStep)
+            .filter(RSA::isMillerRabin)
+            .limit(100)
+            .collect(Collectors.toList());
+        Long endTime = nanoTime();
+        primes.forEach(System.out::println);
+        System.out.println(String.format("it took %d ms to generate 100 %d-bit primes, avg: %d ms/prime", (endTime-startTime)/1000000, bitSize, (endTime-startTime)/(1000000*100)));
+        return primes;
     }
 }
--- project1/src/RSA/RSA.java
+++ project1/src/RSA/RSA.java
@@ -1,80 +1,96 @@
-package RSA;
-/**
-    author: Simon Farre
-    email: simon.farre.x@gmail.com
-    Written 2018 for a lab at school.
+/*
+* author: Simon Farre
+* email: simon.farre.x@gmail.com
+* Written 2018 for a lab at school.
 */
+package RSA;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadMXBean;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import static java.math.BigInteger.*;
-import static java.lang.Math.pow;
+import static java.lang.System.nanoTime;
+import static java.lang.System.currentTimeMillis;
 interface BigIntGenerate {
     BigInteger randomStep(BigInteger b);
 }
 
 public class RSA {
 
-    public BigInteger p, q, d;
-    public long exp;    // exponent e
+    private static final BigInteger TWO = ONE.add(ONE);
+
+    private BigInteger p, q, d;
+    private BigInteger e = TWO.pow(16).add(ONE); // 2^16 + 1
+    private BigInteger N;
+
+    @Override
+    public String toString() {
+        return "RSA {" +
+              "\n" + "Size: " + p.bitLength()   + "\tp=" + p +
+            ", \n" + "Size: " + q.bitLength() + "\tq=" + q +
+            ", \n" + "Size: " + d.bitLength() + "\td=" + d +
+            ", \n" + "Size: " + e.bitLength() + "\te=" + e +
+            ", \n" + "Size: " + N.bitLength() + "\tN=" + N +
+            "\n}";
+    }
 
     /**
      * Default constructor. Returns an RSA scheme, with BigIntegers of bitsize 512
      */
-    public RSA() {
-        this.exp = 65537; // 2^16 + 1
+    public RSA() throws Exception {
+        this.e = TWO.pow(16).add(ONE); // 2^16 + 1
         new RSA(512);
     }
 
-    /**
-     * Construct a RSA scheme with parameters P and Q.
-     * @param P
-     * @param Q
-     */
+    public void setE(BigInteger e) { this.e = e; }
 
-    public RSA(Long p, Long q)  {
-        new RSA(valueOf(p), valueOf(q));
-    }
-
-    public RSA(BigInteger P, BigInteger Q) {
+    private RSA(BigInteger P, BigInteger Q) throws Exception {
+        this.p = P;
+        this.q = Q;
         BigInteger m = (P.subtract(ONE))
                         .multiply
                         (Q.subtract(ONE));
-        d = modInversem(valueOf(exp), m);
+        this.N = P.multiply(Q);
+        d = modInversem(e, m);
+        assert(e.multiply(d).mod(m).compareTo(ONE) == 0);
+        if(!(e.multiply(d).mod(m).compareTo(ONE) == 0)) {
+            System.out.println("Construction of RSA scheme failed");
+            throw new Exception("RSA Construction of RSA scheme failed");
+        }
+        this.N = p.multiply(q);
     }
 
-    public RSA(int bitsize) {
-        Random r_p = new Random(Long.MAX_VALUE ^ System.currentTimeMillis());
-        Random r_q = new Random(Long.MIN_VALUE ^ System.nanoTime());
-        new RSA(new BigInteger(bitsize, r_p), new BigInteger(bitsize, r_q));
+    RSA(int keyBitSize) throws Exception {
+        BigInteger _q;
+        BigInteger _p;
+        do {
+            _q = new BigInteger(keyBitSize, new Random(Long.MAX_VALUE ^ nanoTime()));
+        } while(!isMillerRabin(_q));
+        do {
+            _p = new BigInteger(keyBitSize, new Random(Long.MAX_VALUE ^ currentTimeMillis()));
+        } while(!isMillerRabin(_p));
+        System.out.println("Done generating p and q");
+        this.p = _p;
+        this.q = _q;
+        BigInteger m = (p.subtract(ONE))
+            .multiply
+                (q.subtract(ONE));
+        this.N = p.multiply(q);
+        d = modInversem(e, m);
+        assert((e.multiply(d)).mod(m).compareTo(ONE) == 0);
+        if(!(e.multiply(d).mod(m).compareTo(ONE) == 0)) {
+            System.out.println("Construction of RSA scheme failed");
+            throw new Exception("RSA Construction of RSA scheme failed");
+        }
+        this.N = p.multiply(q);
     }
 
-    public static final BigInteger TWO = ONE.add(ONE);
-    static List<BigInteger> generate_100_primes(int bitSize, BigIntGenerate big) {
-        BigInteger begin;
-        do
-            begin = new BigInteger(bitSize, new Random(System.nanoTime() ^ Long.MAX_VALUE));
-        while(begin.getLowestSetBit() != 0);
-        Long startTime = System.nanoTime();
-        List<BigInteger> primes = Stream.iterate(begin, big::randomStep)
-                .filter(RSA::isMillerRabin)
-                .limit(100)
-                .collect(Collectors.toList());
-        Long endTime = System.nanoTime();
-        primes.forEach(System.out::println);
-        System.out.println(String.format("it took %d ms to generate 100 %d-bit primes, avg: %d ms/prime", (endTime-startTime)/1000000, bitSize, (endTime-startTime)/(1000000*100)));
-        return primes;
-    }
-    private static boolean isMillerRabin(BigInteger n) {
+    static boolean isMillerRabin(BigInteger n) {
         if(n.getLowestSetBit() != 0) {
-            System.out.println("isMillerRabin: false");
             return false;
         }
-        assert n.getLowestSetBit() == 0;
+        assert(n.getLowestSetBit() == 0);
         int idx = 0;
         BigInteger aBasesBig[] = new BigInteger[20];
         for (int i = 0; i < aBasesBig.length; i++) {
@@ -82,7 +98,7 @@ public class RSA {
             do
                 aBasesBig[i] = new BigInteger(n.bitLength(),
                     new Random(
-                        new Random(Long.MAX_VALUE ^ n.bitLength() ^ System.currentTimeMillis())
+                        new Random(Long.MAX_VALUE ^ n.bitLength() ^ currentTimeMillis())
                             .nextLong())); while(aBasesBig[i].compareTo(n) > 0);
         }
         // nMinusOne => n - 1 = 2^r * s
@@ -90,7 +106,7 @@ public class RSA {
         // r is factor of 2. Since numbers are represented in two's complement, it means that
         // the lowest set bit of n, is the exponent of 2^r, i.e. r
         int r = 0;
-        BigInteger s = ZERO;
+        BigInteger s;
         do {
             s = nMinusOne.shiftRight(r);
             // first shift is a no-op. This way we don't have to go s % 2 every loop. But if
@@ -100,15 +116,15 @@ public class RSA {
         } while(s.getLowestSetBit() != 0); // if lowest set bit is not 0, it means we have an even number. We need an odd. keep on.
         {
         Witnesses:
-            for (BigInteger aBase = aBasesBig[idx]; idx < 20; aBase = aBasesBig[idx++]) {
+             for (BigInteger aBase = aBasesBig[idx]; idx < 20; aBase = aBasesBig[idx++]) {
                 BigInteger x = aBase.modPow(s , n);
-                if (x.equals(ONE) || x.equals(nMinusOne))
-                    continue Witnesses; // a is _not_ a witness for the compositeness
+                if (x.compareTo(ONE) == 0 || x.compareTo(nMinusOne) == 0)
+                    continue; // a is _not_ a witness for the compositeness
                 for(int j = 0; j < r; ++j) {
                     x = x.modPow(TWO, n);
-                    if(x.equals(ONE))
+                    if(x.compareTo(ONE) == 0)
                         return false;
-                    if(x.equals(nMinusOne))
+                    if(x.compareTo(nMinusOne) == 0)
                         continue Witnesses;
                 }
                 return false;
@@ -117,32 +133,14 @@ public class RSA {
         }
     }
 
-    public BigInteger eea(BigInteger a, BigInteger m) {
-        BigInteger u1, u2, d1, v1, v2, d2;
-        d1 = m;
-        d2 = a;
-        u1 = v2 = ONE;
-        u2 = v1 = ZERO;
-
-        while(d2.compareTo(ZERO) != 0) {
-            BigInteger q = d1.divide(d2);
-            BigInteger t1 = u1.subtract(q.multiply(u2));
-            BigInteger t2 = v1.subtract(q.multiply(v2));
-            BigInteger t3 = d1.subtract(q.multiply(d2));
-
-            u1 = u2; v1 = v2; d1 = d2;
-            u2 = t1; v2 = t2; d2 = t3;
-        }
-        return ZERO;
-    }
-
+    public BigInteger getN() { return this.N;}
     public static BigInteger modInversem(BigInteger a, BigInteger m) {
-        BigInteger d1, d2, v1, v2;
+        BigInteger d1, v1, v2;
         d1 = m;
-        d2 = a;
         v1 = ZERO; v2 = ONE;
-        BigInteger q, t1,t2,t3;
-        for (d2 = a; d2.compareTo(ZERO) != 0; d2 = t3) {
+        BigInteger q, t2,
+            t3;
+        for (BigInteger d2 = a; d2.compareTo(ZERO) != 0; d2 = t3) {
             q = d1.divide(d2);
             t2 = v1.subtract(q.multiply(v2));
             t3 = d1.subtract(q.multiply(d2));
@@ -151,8 +149,13 @@ public class RSA {
             v2 = t2;
         }
         BigInteger v = v1;
-        BigInteger d = d1;
         if(v.compareTo(ZERO) < 0) return v.add(m);
         return v;
+    }
+    public BigInteger encrypt(BigInteger s) {
+        return s.modPow(e, N);
+    }
+    public BigInteger decrypt(BigInteger c) {
+        return c.modPow(this.d, N);
     }
 }
